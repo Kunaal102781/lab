474eea59a41239705e6ec2569bfc98bcb5c1f9c6
kunaljain <kunal.jain@bfhl.in>
2025-02-23 20:43:58 +0530

    remove comment

--- mimo_with_detector.m
+++ mimo_with_detector.m
@@ -6,10 +6,10 @@ nRx = 2;
 for ii = 1:length(Eb_N0_dB)
  
     ip = rand(1,N)>0.5; 
-    s = 2*ip-1; % BPSK modulation 0 -> -1; 1 -> 0
+    s = 2*ip-1; 
  
-    sMod = kron(s,ones(nRx,1)); % 
-    sMod = reshape(sMod,[nRx,nTx,N/nTx]); % grouping in [nRx,nTx,N/NTx] matrix
+    sMod = kron(s,ones(nRx,1)); 
+    sMod = reshape(sMod,[nRx,nTx,N/nTx]); 
  
     h = 1/sqrt(2)*[randn(nRx,nTx,N/nTx) + j*randn(nRx,nTx,N/nTx)]; 
     n = 1/sqrt(2)*[randn(nRx,N/nTx) + j*randn(nRx,N/nTx)]; 
@@ -18,10 +18,10 @@ for ii = 1:length(Eb_N0_dB)
     y = squeeze(sum(h.*sMod,2)) + 10^(-Eb_N0_dB(ii)/20)*n;
  
     hCof = zeros(2,2,N/nTx)  ; 
-    hCof(1,1,:) = sum(h(:,2,:).*conj(h(:,2,:)),1);  % d term
-    hCof(2,2,:) = sum(h(:,1,:).*conj(h(:,1,:)),1);  % a term
-    hCof(2,1,:) = -sum(h(:,2,:).*conj(h(:,1,:)),1); % c term
-    hCof(1,2,:) = -sum(h(:,1,:).*conj(h(:,2,:)),1); % b term
+    hCof(1,1,:) = sum(h(:,2,:).*conj(h(:,2,:)),1);  
+    hCof(2,2,:) = sum(h(:,1,:).*conj(h(:,1,:)),1);  
+    hCof(2,1,:) = -sum(h(:,2,:).*conj(h(:,1,:)),1); 
+    hCof(1,2,:) = -sum(h(:,1,:).*conj(h(:,2,:)),1); 
     hDen = ((hCof(1,1,:).*hCof(2,2,:)) - (hCof(1,2,:).*hCof(2,1,:)));
     hDen = reshape(kron(reshape(hDen,1,N/nTx),ones(2,2)),2,2,N/nTx);  
     hInv = hCof./hDen; % inv(H^H*H)
